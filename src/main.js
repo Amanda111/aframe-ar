856cfc2424acc3fafce268abede211295bd4dc61
Amanda111 <631551773@qq.com>
2017-08-20 23:40:50 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import aframe from 'aframe'
+
+import THREE from 'three'
+
+// import 'aframe-extras'
+
+import '../loader/gtlfmodel.js'
+
+// import GLTF2Loader from 'three-gltf2-loader'
+
+// GLTF2Loader(THREE)
+
+// console.log(typeof THREE.GLTF2Loader)
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/main.js'),
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 8001
+  }
+}
